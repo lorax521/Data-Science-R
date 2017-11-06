1e7f5db529db3a131912a3a5347082dcf43246fb
James Raines <jraines521@gmail.com>
2017-11-06 08:30:38 -0700

    Update and rename mvlm3Covariates.R to mvlm_rank3.R

--- mvlm_rank3.R
+++ mvlm_rank3.R
@@ -1,4 +1,4 @@
-rank_3covariates <- function(dep, inds) {
+mvlm_rank3 <- function(dep, inds) {
   "
   Ranks permutations of any three covariates for a multivariate linear regression model.
   The dependent and indepedent variables should be stored as seperate columns in a single dataframe.
@@ -17,11 +17,11 @@ rank_3covariates <- function(dep, inds) {
   }
   
   testMVLM <- function(dat) {
-    dep <- grep(as.character(unlist(dat[1])), names(vote))
-    ind1 <- grep(as.character(unlist(dat[2])), names(vote))
-    ind2 <- grep(as.character(unlist(dat[3])), names(vote))
-    ind3 <- grep(as.character(unlist(dat[4])), names(vote))
-    model.mvlm <- glm(unlist(vote@data[dep])~unlist(vote@data[ind1])+unlist(vote@data[ind2])+unlist(vote@data[ind3]))
+    dep1 <- grep(as.character(unlist(dat[1])), names(dep))
+    ind1 <- grep(as.character(unlist(dat[2])), names(inds))
+    ind2 <- grep(as.character(unlist(dat[3])), names(inds))
+    ind3 <- grep(as.character(unlist(dat[4])), names(inds))
+    model.mvlm <- glm(unlist(dep[dep1])~unlist(inds[ind1])+unlist(inds[ind2])+unlist(inds[ind3]))
     s <- summary(model.mvlm, corr=T)
     dfupdate <- rbind(s$aic, as.character(unlist(dat[2])), as.character(unlist(dat[3])), as.character(unlist(dat[4])))
     return(dfupdate)
@@ -32,10 +32,9 @@ rank_3covariates <- function(dep, inds) {
   dat2 <- dat[!duplicated(dat.sort),]
   dat3 <- dat2[apply(dat2, 1, ifTrue),]
   
-  dfout <- data.frame()
-  
-  dfout <- rbind(dfout, t(apply(dat3, 1, testMVLM)))
-  colnames(dfout)<- c('aic', 'covariate1', 'covariate2', 'covariate3')
-  df <- dfout[order(dfout$aic),]
-  return(df)
+  df <- data.frame()
+  df <- rbind(df, t(apply(dat3, 1, testMVLM)))
+  colnames(df)<- c('aic', 'covariate1', 'covariate2', 'covariate3')
+  dfout <- df[order(df$aic),]
+  return(dfout)
 }
